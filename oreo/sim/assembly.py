819289e30cc8b4fd351fd1f9bffbea177caf22bc
Jack Zhu <jack.jz.zhu@gmail.com>
2020-02-24 21:08:33 -0500

    Re-purposing assembly.py as a test bed for new features to add to Oreo class

--- oreo/sim/assembly.py
+++ oreo/sim/assembly.py
@@ -2,42 +2,51 @@ import pybullet as p
 import time
 import pybullet_data
 from scipy.spatial.transform import Rotation as R
+import logging
+
+#Setup debug prints
+useDebug = True
+if (useDebug):
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
+    logging.info('LOG_LEVEL: DEBUG')
+else:
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(message)s')
+    logging.info('LOG_LEVEL: INFO')
 
 # Setup environment
 physicsClient = p.connect(p.GUI)
 #physicsClient = p.connect(p.DIRECT)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-#planeId = p.loadURDF("plane.urdf")
 p.setAdditionalSearchPath("/home/oreo/Documents/oreo_sim/oreo/sim")
 p.setRealTimeSimulation(0)
 startPos = [0,0,0]
 startOrn = p.getQuaternionFromEuler([0,0,0])
-urdf_flags = p.URDF_USE_INERTIA_FROM_FILE
+urdf_flags = p.URDF_USE_INERTIA_FROM_FILE | p.URDF_MAINTAIN_LINK_ORDER
 linkage = p.loadURDF("assembly.urdf", startPos, startOrn, useFixedBase = 1, flags = urdf_flags)
 
 # See startup info
 numJoints = p.getNumJoints(linkage)
-print("There are " + str(numJoints) + " links in file")
+logging.info("There are %d links in file\n", numJoints)
 jointInfoList = {}
 for i in range(numJoints):
     jointInfo = p.getJointInfo(linkage, i)
     jointInfoList[jointInfo[1].decode('UTF-8')] = jointInfo[0]
     state = p.getLinkState(linkage, jointInfo[0])
     coll_data = p.getCollisionShapeData(linkage, jointInfo[0])
-    print(jointInfo[1].decode('UTF-8'))
-    print("com frame (posn & orn)" +  str(state[0]) + str(p.getEulerFromQuaternion(state[1])))
-    print("inertial offset (posn & orn)" + str(state[2]) + str(p.getEulerFromQuaternion(state[3])))
-    print("link frame (posn & orn)" + str(state[4]) + str(p.getEulerFromQuaternion(state[5])))
-    print("type " + str(jointInfo[2]))
-    print("collision (path, posn, orn)" + str(coll_data))
-    print()
-    print()
+    logging.debug("%s %d", jointInfo[1].decode('UTF-8'), i)
+    logging.debug("com frame (posn & orn) %s %s", str(state[0]), str(p.getEulerFromQuaternion(state[1])))
+    logging.debug("inertial offset (posn & orn) %s %s", str(state[2]), str(p.getEulerFromQuaternion(state[3])))
+    logging.debug("link frame (posn & orn) %s %s", str(state[4]), str(p.getEulerFromQuaternion(state[5])))
+    logging.debug("type %s", str(jointInfo[2]))
+    logging.debug("collision (path, posn, orn) %s \n\n", str(coll_data))
 
 # Set collision characteristics
 p.setCollisionFilterPair(linkage, linkage, jointInfoList['dogbone_joint_far_left'], jointInfoList['left_eye_joint'], 1)
 p.setCollisionFilterPair(linkage, linkage, jointInfoList['dogbone_joint_mid_left'], jointInfoList['left_eye_joint'], 1)
 p.setCollisionFilterPair(linkage, linkage, jointInfoList['dogbone_joint_mid_right'], jointInfoList['right_eye_joint'], 1)
 p.setCollisionFilterPair(linkage, linkage, jointInfoList['dogbone_joint_far_right'], jointInfoList['right_eye_joint'], 1)
+p.setCollisionFilterPair(linkage, linkage, jointInfoList['dogbone_joint_far_right'], jointInfoList['pitch_piece_joint'], 1)
+p.setCollisionFilterPair(linkage, linkage, jointInfoList['dogbone_joint_far_left'], jointInfoList['pitch_piece_joint'], 1)
+p.setCollisionFilterPair(linkage, linkage, jointInfoList['skull_joint'], jointInfoList['pitch_piece_joint'], 1)
 
 # Set dynamics of the links (friction etc.)
 
@@ -67,18 +76,10 @@ p.resetJointStateMultiDof(linkage, jointInfoList['dogbone_joint_far_right'], tar
 # [16.47e-3, 14.47e-3, 19.77e-3] [30.25e-3, 0, 0] mid right 
 constraint_type = p.JOINT_POINT2POINT
 eyeState = p.getLinkState(linkage, jointInfoList['right_eye_joint'])
-print("eye state")
-print("com frame (posn & orn)" +  str(eyeState[0]) + str(eyeState[1]))
-print("inertial offset (posn & orn)" + str(eyeState[2]) + str(eyeState[3]))
-print("link frame (posn & orn)" + str(eyeState[4]) + str(eyeState[5]))
-print()
-print()
-
 eyeRot = R.from_quat(eyeState[1])
 eyePos = eyeRot.apply([16.47e-3, 12.81e-3, 19.77e-3]).tolist()
-print("eye constraint: " + str(eyePos))
+logging.debug("eye constraint: %s\n", str(eyePos))
 dogbonePos = [30.25e-3, 0, 0]
-
 
 cid_far_left = p.createConstraint(parentBodyUniqueId = linkage, parentLinkIndex = jointInfoList['left_eye_joint'], childBodyUniqueId=linkage, childLinkIndex = jointInfoList['dogbone_joint_far_left'], jointType = constraint_type, jointAxis = [0, 0, 0], parentFramePosition = [0.015, -0.0016, 0.0245], childFramePosition = dogbonePos)
 p.changeConstraint(cid_far_left, maxForce = 100)
@@ -110,13 +111,8 @@ p.setJointMotorControlMultiDof(linkage, jointInfoList['dogbone_joint_mid_left'],
 p.setJointMotorControlMultiDof(linkage, jointInfoList['dogbone_joint_mid_right'], controlMode = p.POSITION_CONTROL, targetPosition = [0], targetVelocity = [0, 0, 0], positionGain = 0, velocityGain = 1, force = [0,0,0])
 p.setJointMotorControlMultiDof(linkage, jointInfoList['dogbone_joint_far_right'], controlMode = p.POSITION_CONTROL, targetPosition = [0], targetVelocity = [0, 0, 0], positionGain = 0, velocityGain = 1, force = [0,0,0])
 
-p.resetJointStateMultiDof(linkage, jointInfoList['dogbone_joint_far_left'], targetValue = [0, 0, 0.00427])
-p.resetJointStateMultiDof(linkage, jointInfoList['dogbone_joint_mid_left'], targetValue = [0, 0, 0.00427])
-p.resetJointStateMultiDof(linkage, jointInfoList['dogbone_joint_mid_right'], targetValue = [0, 0, -0.00427])
-p.resetJointStateMultiDof(linkage, jointInfoList['dogbone_joint_far_right'], targetValue = [0, 0, -0.00427])
 
 # Create debug window to control joints
-joint_ctrl = {}
 neck_ctrl = p.addUserDebugParameter("neck_joint", -1.57, 1.57, 0)
 pitch_piece_ctrl = p.addUserDebugParameter("pitch_piece_joint", -1.57, 1.57, 0)
 skull_ctrl = p.addUserDebugParameter("skull_joint", -1.57, 1.57, 0)
@@ -125,9 +121,18 @@ linear_motor_mid_left_ctrl = p.addUserDebugParameter("motor_rod_joint_mid_left",
 linear_motor_far_right_ctrl = p.addUserDebugParameter("motor_rod_joint_far_right", -0.1, 0.1, 0)
 linear_motor_mid_right_ctrl = p.addUserDebugParameter("motor_rod_joint_mid_right", -0.1, 0.1, 0)
 
+# Print contact
+
 # Manual Control
 p.setGravity(0,0,-9.8)
-p.setRealTimeSimulation(1)
+
+useRealTime = False
+if useRealTime :
+    p.setRealTimeSimulation(1)
+
+# Logging
+log_id = p.startStateLogging(p.STATE_LOGGING_GENERIC_ROBOT, "oreo_log.txt")
+
 while (1):
     # Get input from sliders
     neck_posn = p.readUserDebugParameter(neck_ctrl)
@@ -147,6 +152,11 @@ while (1):
     p.setJointMotorControl2(linkage, jointInfoList['linear_motor_rod_joint_far_right'], targetPosition = linear_motor_far_right_posn, controlMode = ctrl_mode, force = maxforce)
     p.setJointMotorControl2(linkage, jointInfoList['skull_joint'], targetPosition = skull_posn, controlMode = ctrl_mode, force = maxforce)
     
+    if useRealTime == False :
+        #events = p.getMouseEvents()
+        #for e in events :
+            #if (e[0] == p.MOUSE_BUTTON_EVENT) && ()
+        p.stepSimulation()
 
-
+p.stopStateLogging(log_id)
 p.disconnect()
