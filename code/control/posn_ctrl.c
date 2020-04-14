420e9fb8c898f61aa58396d7f793b5461ea7259a
Jack Zhu <jack.jz.zhu@gmail.com>
2020-04-13 23:15:18 -0400

    Modifying how setting target posn through UI works (separate options for neck and eye) and adding setting target posn by file; basic testing done for both

--- code/control/posn_ctrl.c
+++ code/control/posn_ctrl.c
@@ -12,11 +12,14 @@
 #include "TML_RS232_lib/include/TML_RS232_lib.h"
 #include "sock_interface/sock.h"
 
+#define DEBUG_MENU
+
 static eyeCalData_t* eyeCalData = NULL;
 static eyeData_t* eyeData = NULL;
 static neckData_t* neckData = NULL;
+#ifndef DEBUG_MENU
 static double eyeCalPos[NUM_EYE_AXIS] = {10, 10, 10, 10};
-static double eyeOffset[NUM_EYE_AXIS] = {0};
+#endif
 
 #define EYE_POS_LIM                 {.low = 0, .high = 10}
 #define NECK_POS_LIM                {.low = 0, .high = 10}
@@ -26,6 +29,7 @@ static double eyeOffset[NUM_EYE_AXIS] = {0};
 #define NECK_TORQUE_LIM             {.low = 0, .high = 10}
 #define NECK_YAW_TORQUE_LIM         {.low = 0, .high = 10}
 #define CONTROL_THREAD_PRIO         (31)
+#define BUFSIZE                     (80)
 
 static pid_loop_t pid_neck_yaw = {
     .param = {.kp = 0, .ki = 0, .kd = 0, .integ_lim = INTEG_LIM, .cmd_lim = NECK_YAW_TORQUE_LIM, .pos_lim = NECK_YAW_POS_LIM},
@@ -62,28 +66,6 @@ static pid_loop_t pid_eye_pitch_right = {
     .errSum = 0, .prevErr = 0, .target = 0, .prevTime = 0
 };
 
-static void print_cmd_menu()
-{
-    printf("\n****CMD MENU****\n");
-    printf("[d] set desired posn\n");
-    printf("[s] start torque ctrl\n");
-    printf("[t] display target posns\n");
-    printf("[q] quit\n");
-    printf("[h] help\n");
-    printf("****CMD MENU****\n");
-}
-
-typedef enum {
-    MENU_NECK_YAW = 1,
-    MENU_NECK_PITCH,
-    MENU_NECK_ROLL,
-    MENU_EYE_YAW_LEFT,
-    MENU_EYE_YAW_RIGHT,
-    MENU_EYE_PITCH_LEFT,
-    MENU_EYE_PITCH_RIGHT,
-    NUM_MOTORS = MENU_EYE_PITCH_RIGHT,
-} motor_menu_id_t;
-
 typedef struct {
     pid_loop_t* pid_loop;
     pos_t* feedback;
@@ -114,8 +96,6 @@ static motor_ctrl_t eye_ctrl[NUM_EYE_AXIS] = {
         .callback = &SetEyeForce, .axis_id = EYE_PITCH_RIGHT_AXIS, .name = "Eye Pitch Right"},
 };
 
-static pthread_t loop_handle[NUM_MOTORS]; 
-
 void* ControlLoop(void* input)
 {
     int err;
@@ -144,7 +124,7 @@ void* ControlLoop(void* input)
             pthread_mutex_lock(&ctrl->feedback->mutex);
             time = ctrl->feedback->time;
             pos = ctrl->feedback->pos - ctrl->pos_offset;
-            pthread_mutex_unlock(&ctrl->feedback->time);
+            pthread_mutex_unlock(&ctrl->feedback->mutex);
         } while(time == ctrl->pid_loop->prevTime);
 
         torque = UpdateLoop(ctrl->pid_loop, pos, time);
@@ -155,22 +135,108 @@ void* ControlLoop(void* input)
     pthread_exit(NULL);
 }
 
-static void print_motor_menu()
+static void print_cmd_menu()
 {
-    printf("\n****MOTOR MENU****\n");
+    printf("\n****CMD MENU****\n");
+    printf("[e] set eye target posn\n");
+    printf("[n] set neck target posn\n");
+    printf("[f] set target posn from file\n");
+    printf("[t] display target posns\n");
+    printf("[s] start ctrl loops\n");
+    printf("[q] quit\n");
+    printf("[h] help\n");
+    printf("****CMD MENU****\n");
+}
+
+static void print_eye_menu()
+{
+    printf("\n****EYE MOTOR MENU****\n");
+    printf("[1] eye_yaw_left\n");
+    printf("[2] eye_yaw_right\n");
+    printf("[3] eye_pitch_left\n");
+    printf("[4] eye_pitch_right\n");
+    printf("[q] back to main\n");
+    printf("****EYE MOTOR MENU****\n");
+}
+
+static void print_neck_menu()
+{
+    printf("\n****NECK MOTOR MENU****\n");
     printf("[1] neck_yaw\n");
     printf("[2] neck_pitch\n");
     printf("[3] neck_roll\n");
-    printf("[4] eye_yaw_left\n");
-    printf("[5] eye_yaw_right\n");
-    printf("[6] eye_pitch_left\n");
-    printf("[7] eye_pitch_right\n");
-    printf("****MOTOR MENU****\n");
+    printf("[q] back to main\n");
+    printf("****NECK MOTOR MENU****\n");
+}
+
+static void read_file()
+{
+    int err = 0;
+    char fileName[BUFSIZE], buff[BUFSIZE]; 
+    FILE* file = NULL;
+    double dbuff[NUM_EYE_AXIS] = {0};
+    printf("Enter file name (must be .csv):");
+    if(scanf("%s", fileName) != 1) {
+        printf("Invalid file name input\n");
+        return;
+    }
+    // .csv only
+    if(strstr(fileName, ".csv") == NULL) {
+        printf("Invalid file type\n");
+        return;
+    }
+    if((file = fopen(fileName, "r")) == NULL) {
+        err = errno;
+        printf("Failed to open file %s with errno=%d\n", fileName, err);
+        return;
+    }
+    // One line for neck & eye each
+    // Expecting following format:
+    // val,val,val\n
+    // val,val,val,val\n
+    for(int i = 0; i < 2; i++) {
+        // Read a line
+        if(fgets(buff, sizeof(buff), file) == NULL) {
+            err = ferror(file);
+            printf("Read error occurred err=%d\n", err);
+            return;
+        }
+        if(strlen(buff)+1 == sizeof(buff)) {
+            printf("Buffer overflow while reading line %d\n", i+1);
+            return;
+        }
+        int entries = sscanf(buff, "%lf,%lf,%lf,%lf\n", &dbuff[0], &dbuff[1], &dbuff[2], &dbuff[3]);
+        if(entries == NUM_NECK_AXIS) {
+            for(int j = 0; j < NUM_NECK_AXIS; j++) {
+                neck_ctrl[j].pid_loop->target = dbuff[j];
+            }
+        } else if(entries == NUM_EYE_AXIS) {
+            for(int j = 0; j < NUM_EYE_AXIS; j++) {
+                eye_ctrl[j].pid_loop->target = dbuff[j];
+            }
+        } else {
+            printf("Unable to parse line %d", i+1);
+        }
+    }
+}
+
+static void display_target_posns()
+{
+    printf("\n****TARGET POSNS****\n");
+    for(int i = 0; i < NUM_NECK_AXIS; i++) {
+        printf("%s %.3f\n", neck_ctrl[i].name, neck_ctrl[i].pid_loop->target);
+    }
+    for(int j = 0; j < NUM_EYE_AXIS; j++) {
+        printf("%s %.3f\n", eye_ctrl[j].name, eye_ctrl[j].pid_loop->target);
+    }
+    printf("****TARGET POSNS****\n");
 }
 
 int main()
 {
+#ifndef DEBUG_MENU
     int err;
+#endif
     eyeCalData = GetEyeCalData();
     eyeData = GetEyeData();
     neckData = GetNeckData();
@@ -182,6 +248,7 @@ int main()
         eye_ctrl[j].feedback = &eyeData->pos[j];
     }
 
+#ifndef DEBUG_MENU
     // Wait for neck to run start up
     for(int i = 0; i < NUM_NECK_AXIS; i++) {
         printf("Neck axis %d startup routine....", i+1);
@@ -193,7 +260,7 @@ int main()
 
     // Eye calibration routine
     for(int i = 0; i < NUM_EYE_AXIS; i++) {
-        printf("Eye axis %d calibration routine....");
+        printf("Eye axis %d calibration routine....", i+1);
         StartEyeCal(i, eyeCalPos[i]);
 
         // May need to call calibration module here
@@ -205,52 +272,71 @@ int main()
         // Get the position offset 
         eye_ctrl[i].pos_offset = eyeCalData->offset[i];
     }
+    #endif
 
+    int c;
+    double data;
+    print_cmd_menu();
     while(1) {
-        int c;
-        double data;
         while ((c = getchar()) == '\n' || c == EOF) { }
         switch(c) {
-            case 'd':
-                // set desired posn
-                print_motor_menu();
+            case 'e':
+                // set eye target posn
+                print_eye_menu();
                 while ((c = getchar()) == '\n' || c == EOF) { }
-                c = c - '0';                
-                printf("Enter target:");
-                if(scanf("%lf", &data) == 1) {
-                    switch(c) {
-                        case MENU_NECK_YAW:
-                        case MENU_NECK_PITCH:
-                        case MENU_NECK_ROLL:
-                            neck_ctrl[c-MENU_NECK_YAW].pid_loop->target = data;
-                            printf("Neck axis %d target changed to %f", c, data);
-                            break;
-
-                        case MENU_EYE_YAW_LEFT:
-                        case MENU_EYE_YAW_RIGHT:
-                        case MENU_EYE_PITCH_LEFT:
-                        case MENU_EYE_PITCH_RIGHT:
-                            eye_ctrl[c-MENU_EYE_YAW_LEFT].pid_loop->target = data;
-                            printf("Eye axis %d target changed to %f", c-MENU_EYE_YAW_LEFT+1, data);
-                            break;
-                        default:
-                            printf("Invalid motor selection %d\n", c);
-                            break;
+                if((char)c == 'q') {
+                    // back to main menu
+                    break;
+                }
+                c = c - '0';
+                if(c > 0 && c <= NUM_EYE_AXIS) {
+                    printf("Enter target:");
+                    if(scanf("%lf", &data) == 1) {
+                        eye_ctrl[c-1].pid_loop->target = data;
+                    } else {
+                        printf("Invalid target posn\n");
                     }
                 } else {
-                    printf("Invalid target posn\n");
+                    printf("Invalid eye axis id\n");
                 }
+                display_target_posns();
+                break;
+
+            case 'n':
+                // set neck target posn
+                print_neck_menu();
+                while ((c = getchar()) == '\n' || c == EOF) { }
+                if((char)c == 'q') {
+                    // back to main menu
+                    break;
+                }
+                c = c - '0';
+                if(c > 0 && c <= NUM_NECK_AXIS) {
+                    printf("Enter target:");
+                    if(scanf("%lf", &data) == 1) {
+                        neck_ctrl[c-1].pid_loop->target = data;
+                    } else {
+                        printf("Invalid target posn\n");
+                    }
+                } else {
+                    printf("Invalid eye axis id\n");
+                }
+                display_target_posns();
+                break;
+
+            case 'f':
+                // Load target positions from file
+                read_file();
+                display_target_posns();
                 break;
 
             case 't':
                 // display target posns
-                printf("\nTARGET POSNS\n");
-                for(int i = 0; i < NUM_NECK_AXIS; i++) {
-                    printf("%s %lf\n", neck_ctrl[i].name, neck_ctrl[i].pid_loop->target);
-                }
+                display_target_posns();
                 break;
 
             case 's':
+#ifndef DEBUG_MENU
                 // Set motion modes
                 InitNeckTorqueCtrl();
                 InitEyeForceCtrl();
@@ -294,6 +380,7 @@ int main()
                 }
                 DisableEyeCtrl();
                 DisableNeckCtrl();
+#endif                
                 break;
 
             case 'q':
